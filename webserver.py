bd1ca11d144fb40992975e9c7647c9e971fcee5e
seraermolenko <sera.ermolenko@gmail.com>
2025-10-23 13:15:52 -0700

    all messages work now

--- webserver.py
+++ webserver.py
@@ -5,21 +5,22 @@ from email.utils import formatdate, parsedate_to_datetime
 
 # testing
 # 200 
-# curl http://127.0.0.1:8080/test.html
+# curl -i http://127.0.0.1:8080/test.html
 
 # 304
 # (to check last mod date of test and sjut if needed): stat -f "%Sm" -t "%a, %d %b %Y %H:%M:%S GMT" test.html
-# curl http://127.0.0.1:8080/test.html --header "If-Modified-Since: Wed, 22 Oct 2025 16:00:00 GMT"
+# ensure you disable cache (in devtools network for chrome) and also Empty Cache and Hard Reload (press n hold reload)
+# curl -i http://127.0.0.1:8080/test.html --header "If-Modified-Since: Wed, 22 Oct 2025 16:00:00 GMT"
 
 # 403
 # u gotta create a secret file and change its permisions cuz git wont let me push w it (chmod 000 teehee.txt)
 # I just added a dummy teehee file with 644 so that I could push
-# curl http://127.0.0.1:8080/teehee.txt
+# curl -i http://127.0.0.1:8080/teehee.txt
 
 # 404 
-# curl http://127.0.0.1:8080/doesnotexist.html
+# curl -i http://127.0.0.1:8080/doesnotexist.html
 # 505 
-# curl "http://127.0.0.1:8080/test.html?v=2"
+# curl -1 "http://127.0.0.1:8080/test.html?v=2"
 
 HOST = "127.0.0.1" 
 PORT = 8080  
@@ -40,6 +41,7 @@ def build_response(code, body=b"", content_type="text/plain"):
         f"Content-Length: {len(body)}",
         f"Date: {formatdate(time.time(), usegmt=True)}",
         f"Content-Type: {content_type}",
+        "Cache-Control: no-cache", 
         "Connection: close",
         "",
         ""
@@ -68,27 +70,34 @@ def handle_client(connection):
         version = "HTTP/2.0"
 
     if version != "HTTP/1.1":
-        connection.sendall(build_response(505, b"HTTP Version Not Supported"))
+        connection.sendall(build_response(505, b" Error 505: HTTP Version Not Supported"))
         return
 
     file_path = os.path.join(WEB_ROOT, path.lstrip("/"))
 
     if not os.path.exists(file_path):
-        connection.sendall(build_response(404, b"Not Found"))
+        connection.sendall(build_response(404, b"Error 404: Not Found"))
         return
 
     if not os.access(file_path, os.R_OK):
-        connection.sendall(build_response(403, b"Forbiden"))
+        connection.sendall(build_response(403, b"Error 403: Forbidden"))
         return
 
     headers = request.split("\r\n")[1:]
+    print("headers:")
+    print(headers)
     if_mod_since_header = None
     for h in headers:
         if h.lower().startswith("if-modified-since:"):
             if_mod_since_header = h.split(":", 1)[1].strip()
             break
+    print("if_mod_since_header:")
+    print(if_mod_since_header)
 
     last_modified = os.path.getmtime(file_path)
+
+    print("last_modified:")
+    print(last_modified)
 
     if if_mod_since_header:
         if_mod_since_dt = parsedate_to_datetime(if_mod_since_header)
@@ -109,7 +118,7 @@ def run():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # server socket
         s.bind((HOST, PORT))
         s.listen(5)
-        print(f"🌐 Serving on http://{HOST}:{PORT}")
+        print(f"Serving on http://{HOST}:{PORT}")
 
 
         while True: # server always running
